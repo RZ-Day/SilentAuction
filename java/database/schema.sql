32ea599d97b70104ee5695d29a447d770ad319e7
Zin Ma <zin.ma7@gmail.com>
2023-08-07 13:13:58 -0400

    remove sample database setup

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -10,22 +10,5 @@ CREATE TABLE users (
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
 );
 
-CREATE TABLE categories (
-	category_id serial,
-	category_name varchar (500) NOT NULL,
-	CONSTRAINT PK_categories PRIMARY KEY (category_id)
-
-);
-
-CREATE TABLE items (
-	item_id serial,
-	item_number INT NOT NULL,
-	item_description varchar(500) NOT NULL,
-	starting_bid_amount DECIMAL (10, 2) NOT NULL,
-	category_id INT NOT NULL,
-	CONSTRAINT PK_item_id PRIMARY KEY(item_id),
-	CONSTRAINT FK_category_id FOREIGN KEY (category_id) REFERENCES categories (category_id)
-);
-
 
 COMMIT TRANSACTION;
